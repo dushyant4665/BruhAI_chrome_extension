cd3ea22e6087e5257e34270c760db30a6038cc7f
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2025-03-08 07:20:39 -0800

    Update Cache.js

--- backend/src/db/models/Cache.js
+++ backend/src/db/models/Cache.js
@@ -17,6 +17,4 @@ const cacheSchema = new mongoose.Schema({
     index: { expires: '1h' }
   }
 });
-
-// For models, named export is okay since we're exporting a constant
 export const Cache = mongoose.model('Cache', cacheSchema);
